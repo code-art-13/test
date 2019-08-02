b9002d12509db2db4c102c44148f342ab741e752
Sunil Chakrahari <sc@sc.fios-router.home>
2019-08-02 09:13:15 -0400

    basic tests passing

--- stock-quotes-app/src/components/StockDetail.js
+++ stock-quotes-app/src/components/StockDetail.js
@@ -1,3 +1,8 @@
 //display details when a stock is selected in the stocks grid
-// import React from 'react';
+import React from 'react';
 
+const StockDetail = () => {
+  return (<div>Stock Detail</div>);
+};
+
+export default StockDetail;
--- stock-quotes-app/src/components/StocksGrid.js
+++ stock-quotes-app/src/components/StocksGrid.js
@@ -1,2 +1,11 @@
 //Add Stocks Grid to display
-// import React from 'react';
+import React from 'react';
+
+const StocksGrid = () => {
+  return (
+  <div>
+      Stocks Grid
+  </div>);
+};
+
+export default StocksGrid;
